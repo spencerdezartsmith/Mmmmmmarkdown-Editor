0bc5c2c39d4ba2dd55546f8bc7a9fdf43375dbb8
Jolly Walia <jwalia@hammer.local>
2017-05-02 10:47:23 -0700

    Setup express static middleware.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 var express = require('express');
 
 var app = express();
+app.use(express.static('public'));
 
 app.set('views', __dirname + '/views');
-
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
