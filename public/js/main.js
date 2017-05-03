ddb326649accc82a160a278c22afbdcf97deb957
Jolly Walia <jwalia@hammer.local>
2017-05-03 11:44:42 -0700

    Built add new file function in jQuery.

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,39 @@
 $(document).ready(() => {
-	let previewField = $('.preview-content')[0];
-  let editorInput = $('textarea.form-control')
+
+  let previewField = $('.preview-content')[0];
+  let editorInput = $('textarea.form-control');
+  let table = $('.table');
+  let newFile = $('.new-file');
+  let fileName = $('.filename');
 
   updateInput(editorInput, previewField);
+  addNewFile(table, newFile, fileName);
+
 });
 
 function updateInput(input, preview) {
-  input.keyup(function() {
-		preview.innerHTML = this.value;
-	});
+  input.keyup(function () {
+    preview.innerHTML = this.value;
+  });
 }
 
-function addNewFile(tableEl, addFileEl) {
-  addFileEl.on('click', function() {
+function addNewFile(tableEl, addFileEl, fileName) {
+  let idx = addFileEl.index(this);
 
-  })
+  addFileEl.on('click', function () {
+    tableEl.find('tr:last').before('<tr><td>untitled.md<span><i class="fa fa-trash" aria-hidden="true"></i></span></td></tr>');
+		fileName.html('untitled.md');
+
+		let newFileName = new Promise((resolve) => {
+			setTimeout( () => {
+				resolve(prompt('Name your markdown file'));
+			}, 1000);
+		});
+
+		newFileName.then( (result) => {
+    	let str = '<tr><td>' + result + '<span><i class="fa fa-trash" aria-hidden="true"></i></span></td></tr>';
+    	tableEl.find('tr:last').prev().replaceWith(str);
+			fileName.text(result);
+		});
+  });
 }
