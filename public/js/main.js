748d1e39e1d67ca64728495fa916361d4136d0e0
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-02 18:16:29 -0700

    Add keyup() listening to preview input

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
-
 $(document).ready(() => {
-  // alert('Hello World');
-  console.log('Check Console');
+	let preview = $('.preview-content')[0];
+  // Update text into preview window in realtime
+	$('textarea.form-control').keyup(function() {
+		preview.innerHTML = this.value;
+	});
 });
